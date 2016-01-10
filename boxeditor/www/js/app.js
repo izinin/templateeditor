d59c08d3d0f7e78053c205ff74337203f7199cc2
Igor Zinin <igor.zinin@gmail.com>
2016-01-10 15:55:02 +0200

    initial draft -- can move boxes

---
+++ boxeditor/www/js/app.js
@@ -0,0 +1,43 @@
+(function() {
+  var app = angular.module('templateEditor', ['draggable']);
+
+  app.controller('editorController', function(){
+    this.template = template;
+  });
+
+  var template = {
+   "width": 1920,
+   "height": 1080,
+   "elements": [
+      {
+         "id": 0,
+         "x": 0,
+         "y": 100,
+         "z": 50,
+         "width": 100,
+         "height": 200,
+         "text": "Sample text"
+      },
+      {
+         "id": 1,
+         "x": 10,
+         "y": 200,
+         "z": 40,
+         "width": 100,
+         "height": 200,
+         "text": "Sample text 2"
+      },
+      {
+         "id": 2,
+         "x": 100,
+         "y": 300,
+         "z": 150,
+         "width": 300,
+         "height": 200,
+         "text": "Sample text 3"
+      }
+   ]
+
+}
+  
+})();
