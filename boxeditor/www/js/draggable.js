83255b108fb4f488ad99bbfd69c95661d77940c4
Igor Zinin <igor.zinin@gmail.com>
2016-01-10 17:41:01 +0200

    linking controller and directive

--- boxeditor/www/js/draggable.js
+++ boxeditor/www/js/draggable.js
@@ -1,38 +1,55 @@
-angular.module('draggable', [])
-.directive('myDraggable', ['$document', function($document) {
-  return {
-    link: function(scope, element, attr) {
-      var startX = 0, startY = 0, x = 0, y = 0;
+(function() {
+    var app = angular.module('draggable', []);   
 
-      element.css({
-       position: 'relative',
-       border: '1px solid red',
-       backgroundColor: 'lightgrey',
-       cursor: 'pointer'
-      });
+    app.directive('myDraggable', ['$document', function($document) {
+        var link = function(scope, element, attr, editor) {
+            var startX = 0,
+                startY = 0,
+                x = 0,
+                y = 0;
 
-      element.on('mousedown', function(event) {
-        // Prevent default dragging of selected content
-        event.preventDefault();
-        startX = event.pageX - x;
-        startY = event.pageY - y;
-        $document.on('mousemove', mousemove);
-        $document.on('mouseup', mouseup);
-      });
+            element.css({
+                position: 'relative',
+                border: '1px solid red',
+                backgroundColor: 'lightgrey',
+                cursor: 'pointer' /*,
+                top: editor.y,
+                left: editor.x,
+                width: editor.width + 'px',
+                height: editor.height + 'px',*/
+                });
 
-      function mousemove(event) {
-        y = event.pageY - startY;
-        x = event.pageX - startX;
-        element.css({
-          top: y + 'px',
-          left:  x + 'px'
-        });
-      }
+            element.on('mousedown', function(event) {
+                // Prevent default dragging of selected content
+                event.preventDefault();
+                startX = event.pageX - x;
+                startY = event.pageY - y;
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
 
-      function mouseup() {
-        $document.off('mousemove', mousemove);
-        $document.off('mouseup', mouseup);
-      }
-    }
-  };
-}]);
+            function mousemove(event) {
+                y = event.pageY - startY;
+                x = event.pageX - startX;
+                element.css({
+                    top: y + 'px',
+                    left: x + 'px'
+                });
+            }
+
+            function mouseup() {
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+                console.log("-->" + editor.template.elements[0].text);
+            }
+        };
+
+        return {
+            restrict:'A',
+            link: link,
+            controller:'editorController',
+            controllerAs:'editor'
+        };
+    }]);
+
+})();
